3f3b04bd2482b5b385f13960afb3f500145e0ead
JonaGarces <jonat.garces@gmail.com>
2022-09-21 18:15:03 +0000

    tictac

---
+++ build/precache-manifest.15fbf6b22e53c96af540af48482e06d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4486e79d6a9ffca2816513f99b2789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf5ac9c0fc4751f51071",
+    "url": "/static/css/main.83158f21.chunk.css"
+  },
+  {
+    "revision": "023fe1b411c6f4863612",
+    "url": "/static/js/2.4fd0d418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4fd0d418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5ac9c0fc4751f51071",
+    "url": "/static/js/main.3d2107d1.chunk.js"
+  },
+  {
+    "revision": "e436760d2a5e82b8f8dd",
+    "url": "/static/js/runtime-main.46f721ec.js"
+  }
+]);
